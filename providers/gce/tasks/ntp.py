9f8f89ea9dd5859ddc303e25d6348727e1351ecc
Tomasz Rybak <tomasz.rybak@post.pl>
2014-04-05 10:07:46 +0200

    Use manifest option to configure NTP server instead of hardcoding it.

--- providers/gce/tasks/ntp.py
+++ providers/gce/tasks/ntp.py
@@ -9,17 +9,13 @@ class FixNTPServer(Task):
 
 	@classmethod
 	def run(cls, info):
-		from common.tools import sed_i
-#		import re
-#		regex = re.compile('^server\s+')
-		ntp_config_path = os.path.join(info.root, 'etc/ntp.conf')
-		server_line = '^\s*server\s'
-		sed_i(ntp_config_path, server_line, '#server')
-#		with open(ntp_config_path) as config_file:
-#			config_lines = config_file.readlines()
-		with open(ntp_config_path, 'w') as config_file:
-#			for line in config_lines:
-#				if regex.match(line) is None:
-#					print >>config_file, line
-			print >>config_file, "# Use server located on the host machine to avoid network traffic"
-			print >>config_file, "server metadata.google.internal iburst"
+		if 'ntpserver' in info.manifest.system:
+			ntpserver = info.manifest.system['ntpserver']
+			if len(ntpserver) > 0:
+				from common.tools import sed_i
+				ntp_config_path = os.path.join(info.root, 'etc/ntp.conf')
+				server_line = '^\s*server\s'
+				sed_i(ntp_config_path, server_line, '#server')
+				with open(ntp_config_path, 'w') as config_file:
+					print >>config_file, "# Use server located on the host machine to avoid network traffic"
+					print >>config_file, "server {ntpserver} iburst".format(ntpserver=ntpserver)
